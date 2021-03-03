c4bdfe99e2d3f73c4f0afe0bc9fa0b3072c297a1
LMO <lmo0731@gmail.com>
2021-03-03 15:04:49 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { loggerMiddleware } from './logger'
 export { Mongo, MongoCollection, IMongo } from './database'
 export { MessageQueue, MessageTopic, IMessage, IMessageQueue, IMessageTopic } from './messaging'
+export { Constraint } from './validation'
