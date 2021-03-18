1d2726e51dcdf080e10660a5ce394ec62da5fb60
LMO <lmo0731@gmail.com>
2021-03-18 11:49:38 +0800

    bug fix

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,14 @@
 import { AsyncLocalStorage } from 'async_hooks'
 import { getLogger } from 'log4js'
-import { ClientSession, Collection, Db, Logger, MongoClient, MongoClientCommonOption, MongoClientOptions, SessionOptions } from 'mongodb'
+import { ClientSession, Collection, Db, Logger, MongoClient, MongoClientCommonOption, MongoClientOptions, SessionOptions, TransactionOptions } from 'mongodb'
 
-const sessionStorage = new AsyncLocalStorage<ClientSession>()
+export const sessionStorage = new AsyncLocalStorage<ClientSession>()
 
 export interface IMongo {
   connect: () => Promise<MongoClient>
   database: (dbname?: string, options?: MongoClientCommonOption) => Promise<Db>
   session: (options?: SessionOptions) => Promise<ClientSession>
-  withTransaction: <T>(callback: (session: ClientSession) => Promise<T>, options?: SessionOptions) => Promise<T>
+  withTransaction: <T>(callback: (session: ClientSession) => Promise<T>, options?: TransactionOptions) => Promise<T>
 }
 
 const logger = getLogger('db')
@@ -34,28 +34,30 @@ export const Mongo = (url: string, db: string, options: MongoClientOptions = { u
     session: async (options) => {
       return client.startSession(options)
     },
-    withTransaction: async<T> (callback: (session: ClientSession) => Promise<T>, options?: SessionOptions): Promise<T> => {
+    withTransaction: async<T> (callback: (session: ClientSession) => Promise<T>, options?: TransactionOptions): Promise<T> => {
       const parentSession = sessionStorage.getStore()
       if (parentSession === undefined) {
-        const session = client.startSession(options)
+        const session = client.startSession()
         logger.debug('starting session')
-        return await sessionStorage.run<Promise<T>>(session, async () => {
-          session.startTransaction()
-          logger.debug('starting transaction')
-          try {
-            const ret = await callback(session)
-            await session.commitTransaction()
-            logger.debug('commit transaction')
-            return ret
-          } catch (e) {
-            await session.abortTransaction()
-            logger.debug('rollback transaction')
-            throw e
-          } finally {
-            await session.endSession()
-            logger.debug('end session')
-          }
-        })
+        try {
+          return await sessionStorage.run<Promise<T>>(session, async () => {
+            session.startTransaction(options)
+            logger.debug('starting transaction')
+            try {
+              const ret = await callback(session)
+              await session.commitTransaction()
+              logger.debug('commit transaction')
+              return ret
+            } catch (e) {
+              await session.abortTransaction()
+              logger.debug('rollback transaction')
+              throw e
+            }
+          })
+        } finally {
+          await session.endSession()
+          logger.debug('end session')
+        }
       } else {
         const ret = await callback(parentSession)
         return ret
