f1ccfe1a16fc8c6b5c2471c313fceb9663311317
lmo0731 <lmo0731@gmail.com>
2021-06-24 17:22:28 +0800

    bug fix koa logger

--- src/logger/koa.ts
+++ src/logger/koa.ts
@@ -6,11 +6,11 @@ export const koaLogger: Koa.Middleware = async (ctx, next) => {
   const logger = getLogger('http')
   const reqId = Math.random()
   const ts = new Date().getTime()
-  ctx.setHeader('X-Request-Id', `${reqId}`)
+  ctx.set('X-Request-Id', `${reqId}`)
   reqIdStorage.run(`${reqId}`, () => {
     logger.info(ctx.method, ctx.originalUrl)
     next().then(() => {
-      logger.info(ctx.statusCode, new Date().getTime() - ts, 'ms')
+      logger.info(ctx.status, new Date().getTime() - ts, 'ms')
     })
   })
 }
