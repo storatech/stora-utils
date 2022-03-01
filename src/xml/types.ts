870812687c127b6efe1fb7bfe67af9376dd2b273
LMO <lmo0731@gmail.com>
2022-03-01 08:40:57 +0800

    xml boolean

--- src/xml/types.ts
+++ src/xml/types.ts
@@ -8,39 +8,79 @@ export type XmlTransformer<T> = () => {
   formatter: XmlFormatter<T>
 }
 
-export type XmlDef<T> = (XmlObject<T> | XmlTransformer<T>)
+export type XmlDef<T> = (XmlObject<T> | XmlTransformer<T | undefined>)
 
 export type XmlObject<T> = {
   [Z in keyof T]?: (T extends [] ? XmlDef<T[number]>: XmlDef<T[Z]>)
 }
 
-export const XmlString = (options?: any): XmlTransformer<string> => {
+export const XmlBoolean = (trueValue: string, falseValue: string): XmlTransformer<boolean | undefined> => {
   return () => {
     return {
       parser: (value) => {
+        if (value === undefined) {
+          return undefined
+        }
+        if (value === trueValue) {
+          return true
+        }
+        if (value === falseValue) {
+          return false
+        }
+      },
+      formatter: (value) => {
+        if (value === undefined) {
+          return undefined
+        }
+        if (value) {
+          return trueValue
+        } else {
+          return falseValue
+        }
+      }
+    }
+  }
+}
+
+export const XmlString = (options?: any): XmlTransformer<string | undefined> => {
+  return () => {
+    return {
+      parser: (value) => {
+        if (value === undefined) {
+          return undefined
+        }
         return value
       },
       formatter: (value) => {
+        if (value === undefined) {
+          return undefined
+        }
         return value
       }
     }
   }
 }
 
-export const XmlNumber = (options?: any): XmlTransformer<number> => {
+export const XmlNumber = (options?: any): XmlTransformer<number | undefined> => {
   return () => {
     return {
       parser: (value) => {
-        return value !== undefined ? parseInt(value, 10) : undefined
+        if (value === undefined) {
+          return undefined
+        }
+        return parseInt(value, 10)
       },
       formatter: (value) => {
-        return value !== undefined ? `${value}` : undefined
+        if (value === undefined) {
+          return undefined
+        }
+        return `${value}`
       }
     }
   }
 }
 
-export const XmlDate = (options?: any): XmlTransformer<Date> => {
+export const XmlDate = (options?: any): XmlTransformer<Date | undefined> => {
   return () => {
     return {
       parser: (value) => {
